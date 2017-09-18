bfe4bb2dd2079b38e8469526f0ebd3ca0d40606e
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-18 17:22:02 +0200

    add to dictionary

---
+++ dictionary/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static Dictionary<string, string> Dictionary;
+
+        static void Main(string[] args)
+        {
+            Dictionary = new Dictionary<string, string>
+            {
+                { "alma", "apple" },
+                { "fa", "tree" }
+            };
+        }
+
+        public static void AddWord(string hungarianWord, string englishWord)
+        {
+            Dictionary.Add(hungarianWord, englishWord);
+        }
+
+        public static string TranslateToEnglish(string hungarian)
+        {
+            
+        }
+
+    }
+}
