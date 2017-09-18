89c258f44bb1c7184bd382b7a645ca96637f8cb8
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-18 17:05:01 +0200

    string email split

---
+++ namefromemail/NameFromEmail/NameFromEmail/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameFromEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter yout email address in the following format: firstName.lastName@exam.com");
+            string email = Console.ReadLine();
+            string Name = NameFromEmail1(email);
+            Console.WriteLine(Name);
+        }
+    }
+
+        public static string NameFromEmail1(string email)
+        {
+        string[] parts = email.Split('.', '@');
+        string firstname = "";
+        string lastname = "";
+        parts[0] = firstname;
+        parts[1] = lastname;
+        string Name = firstname + lastname;
+        return Name;
+
+        }
+}
